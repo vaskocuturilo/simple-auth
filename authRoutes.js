9c8816c8a16bff49694f33039edb13ae0c7cc124
asm@dostavista.ru <wolfstonik>
2021-07-01 10:22:55 +0300

    DV-0004: Add controller.

--- authRoutes.js
+++ authRoutes.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const router = new Router()
+const controller = require('./authController')
 
-router.post('/register')
-router.post('/login')
-router.get('/users')
+router.post('/register', controller.register)
+router.post('/login', controller.login)
+router.get('/users', controller.getUserInformation)
 
 
 module.exports = router

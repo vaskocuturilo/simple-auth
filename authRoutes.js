9818c09fae8d38f50f8b8a2af23fd985f775ccd1
asm@dostavista.ru <wolfstonik>
2021-07-04 01:20:39 +0300

    DV-0007: Add profile endpoint.

--- authRoutes.js
+++ authRoutes.js
@@ -7,10 +7,12 @@ const authMiddleware = require('./middleware/authMiddleware')
 router.post('/register', [
     check('username', 'The username field cannot is empty').notEmpty(),
     check('password', 'The password field cannot is empty').notEmpty(),
-    check('password', 'The password length must be min 4, but not max 8 elements.').isLength({min: 4, max: 8})
+    check('password', 'The password length must be min 4, but not max 8 elements.').isLength({min: 4, max: 8}),
+    check('name', 'The name field cannot is empty').notEmpty()
 ], controller.register)
 router.post('/login', controller.login)
 router.get('/users', authMiddleware, controller.getUserInformation)
+router.get('/profile', authMiddleware, controller.getUserProfile)
 
 
 module.exports = router
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const User = Schema({
     username: {type: String, require: true, unique: true},
     password: {type: String, require: true},
+    name: {type: String, require: true},
     roles: [{type: String, ref: 'Role'}]
 })
 
